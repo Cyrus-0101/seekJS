4f8cc43194f0df817834413915c9b0d4c88e8b97
Toxic254 <cyrusgichuki1998@gmail.com>
2020-01-30 18:45:37 +0300

    Updates

---
+++ precache-manifest.6bc6fbf328e7cf18b38044b0f0e5a2cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475cee56751889bab9aa72d0f3795bc7",
+    "url": "/seekJS/index.html"
+  },
+  {
+    "revision": "19d5d77b33d7ddaeb9e9",
+    "url": "/seekJS/static/css/main.3d6e4415.chunk.css"
+  },
+  {
+    "revision": "7e894316149ac95bff0e",
+    "url": "/seekJS/static/js/2.994afdca.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/seekJS/static/js/2.994afdca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "19d5d77b33d7ddaeb9e9",
+    "url": "/seekJS/static/js/main.41e9ac2e.chunk.js"
+  },
+  {
+    "revision": "4292b39e8392e60fceb7",
+    "url": "/seekJS/static/js/runtime-main.666827c2.js"
+  }
+]);
